c75cb45eed0effa81a67b751a974dd5bc39f3c54
ST2712 <111332821+ST2712@users.noreply.github.com>
2022-09-11 19:49:17 -0500

    Delete desaInt directory

--- desaInt/src/main/java/com/co/software/empresas/desaInt/repository/RepositoryEmpleado.java
+++
@@ -1,6 +0,0 @@
-package com.co.software.empresas.desaInt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryEmpleado extends JpaRepository<EntityEmpleado, Long> {
-}
